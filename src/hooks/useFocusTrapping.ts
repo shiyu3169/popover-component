c4d8a92aceafe964484c0e24a64d1a657a1fb8d2
Shiyu Wang <shiyu3169@gmail.com>
2023-03-02 17:50:45 -0800

    tab between elements

--- src/hooks/useFocusTrapping.ts
+++ src/hooks/useFocusTrapping.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react'
+import { useCallback, useEffect, useRef } from 'react'
 
 // TODO: better focusable query
 const focusableQuery = ':is(input, button, [tab-index])'
@@ -7,8 +7,27 @@ const focusableQuery = ':is(input, button, [tab-index])'
   2. attach keyboard event listener to trap the focus
   3. refocus the trigger after dismissing
 */
+
 export function useFocusTrapping() {
   const ref = useRef<HTMLElement>(null)
+
+  const tabNext = useCallback((e: KeyboardEvent) => {
+    const content = ref.current
+    if (!content) return
+    const focusables = Array.from(content.querySelectorAll(focusableQuery))
+    switch (e.key) {
+      case 'Tab':
+        const lastFocusable = focusables[focusables.length - 1]
+        if (lastFocusable === document.activeElement) {
+          e.preventDefault()
+          // @ts-ignore, TODO: fix typing
+          focusables[0].focus()
+        }
+        break
+      default:
+        break
+    }
+  }, [])
 
   useEffect(() => {
     const content = ref.current
@@ -19,6 +38,11 @@ export function useFocusTrapping() {
     // 1. focus the first focusable
     // @ts-ignore, TODO: fix typing
     focusables[0].focus()
-  }, [])
+    // 2. attach keyboard event listener to trap the focus
+    document.addEventListener('keydown', tabNext)
+    return () => {
+      document.removeEventListener('keydown', tabNext)
+    }
+  }, [tabNext])
   return ref
 }
