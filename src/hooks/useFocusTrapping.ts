9f5c9316ff4d718b8979587814d887ff110786a0
Shiyu Wang <shiyu3169@gmail.com>
2023-03-02 20:18:36 -0800

    click outside

--- src/hooks/useFocusTrapping.ts
+++ src/hooks/useFocusTrapping.ts
@@ -11,18 +11,20 @@ const focusableQuery = ':is(input, button, [tab-index])'
 export function useFocusTrapping() {
   // @ts-ignore, TODO: fix typing
   const refTrigger = useRef<HTMLElement>(document.activeElement)
+  const trigger = refTrigger.current
   const ref = useRef<HTMLElement>(null)
 
   const tabNext = useCallback((e: KeyboardEvent) => {
     const content = ref.current
     if (!content) return
-    const focusables = Array.from(content.querySelectorAll(focusableQuery))
+    const focusables: HTMLElement[] = Array.from(
+      content.querySelectorAll(focusableQuery),
+    )
     switch (e.key) {
       case 'Tab':
         const lastFocusable = focusables[focusables.length - 1]
         if (lastFocusable === document.activeElement) {
           e.preventDefault()
-          // @ts-ignore, TODO: fix typing
           focusables[0].focus()
         }
         break
@@ -35,24 +37,23 @@ export function useFocusTrapping() {
     const content = ref.current
     if (!content) return
 
-    const focusables = Array.from(content.querySelectorAll(focusableQuery))
+    const focusables: HTMLElement[] = Array.from(
+      content.querySelectorAll(focusableQuery),
+    )
 
     // 1. focus the first focusable
-    // @ts-ignore, TODO: fix typing
     focusables[0].focus()
     // 2. attach keyboard event listener to trap the focus
     document.addEventListener('keydown', tabNext)
     return () => {
       document.removeEventListener('keydown', tabNext)
       // 3. refocus the trigger after dismissing
-      const trigger = refTrigger.current
       const currentActiveElement = document.activeElement
       // The current active element will be reset to body if not another trigger is clicked
-      // TODO: this is tricky and can cause bug.
       if (currentActiveElement === document.body) {
         trigger?.focus()
       }
     }
-  }, [tabNext])
+  }, [tabNext, trigger])
   return ref
 }
